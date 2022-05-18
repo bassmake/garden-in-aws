ef7d03171ff0a5a064d60bb2ef956ddf69a419f4
Miroslav Matejovsky <miroslav.matejovsky@gmail.com>
2022-05-18 21:32:34 +0200

    ec2 instance

--- main.tf
+++ main.tf
@@ -11,3 +11,14 @@ provider "aws" {
   region  = "eu-central-1"
   profile = "bsmk"
 }
+
+resource "aws_instance" "garden_linux" {
+  # https://github.com/gardenlinux/gardenlinux/releases/tag/576.9
+  ami           = "ami-04571dd02adce654b"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "GardenLinux"
+  }
+}
+
